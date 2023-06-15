ae57188b3dbdeec87942968cfecc5b551bed0fbe
Rebeca Goncalves <rebecagoncalves14@gmail.com>
2023-06-15 11:24:56 -0400

    Graficas de comparacion entre dos tipos de valores

--- Prueba2.R
+++ Prueba2.R
@@ -2,6 +2,7 @@
 #librería
 library(readxl)
 library(tidyr)
+library(tidyverse)
 
 #Ver en qué directorio estamos parados
 getwd()
@@ -20,4 +21,29 @@ data <- na.omit(data)
 
 #Redondear los valores a dos decimales
 data$valor<- round(data$valor, 2)
+
+##Comparacion entre los dos tipos de valores que tenemos
+# Primer gráfico
+grafico1 <- data %>% filter(tipodevalor == "Número absoluto") %>%
+ggplot(aes(x = anio, y = valor, fill = tipodevalor)) +
+  geom_bar(stat = "identity") +
+  xlab("Años (2001-2021)") +
+  ylab("Número de muertes de mujer") +
+  ggtitle("Muertes de mujeres ocasionadas por su pareja y ex-pareja") +
+  scale_y_continuous(limits=c(0, max(data$valor))) 
+
+# Segundo gráfico
+grafico2 <- data %>% filter(tipodevalor == "Tasa (por cada 100.000 mujeres)") %>%
+  ggplot(aes(x = anio, y = valor, fill = tipodevalor))  +
+  geom_bar(stat = "identity") +
+  xlab("Años (2001-2021)") +
+  ylab("Número de muertes de mujer") + 
+  ggtitle("Muertes de mujeres ocasionadas por su pareja y ex-pareja") +
+  scale_y_continuous(limits=c(0, max(data$valor)))
+  
+
+# Mostrar ambos gráficos juntos
+gridExtra::grid.arrange(grafico1, grafico2, newpage = TRUE)
+
+
 
