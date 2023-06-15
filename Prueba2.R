706f28c5420e8e3e71db9bee43a11bc86bdceb96
Carlos Landaeta <calejandrolg99@gmail.com>
2023-06-15 13:44:16 -0400

    Graficas de linea y explicacion markdown

--- Prueba2.R
+++ Prueba2.R
@@ -3,6 +3,8 @@
 library(readxl)
 library(tidyr)
 library(tidyverse)
+library(ggplot2)
+library(gridExtra)
 
 #Ver en qué directorio estamos parados
 getwd()
@@ -45,5 +47,30 @@ grafico2 <- data %>% filter(tipodevalor == "Tasa (por cada 100.000 mujeres)") %>
 # Mostrar ambos gráficos juntos
 gridExtra::grid.arrange(grafico1, grafico2, newpage = TRUE)
 
+#Colores para los graficos de linea
+colors_vec <- c("#4d4dff", "#00b3b3", "#00cc99", "#b3b300", "#b35900", "#cc99cc", "#ff4d4d", "#808080", "#ff9933", "#6666ff", "#00e6e6", "#b3b3b3", "#ff66ff", "#ffcc99", "#99ff99", "#ff6666", "#b3b300", "#00b3b3", "#ff99cc", "#6666ff", "#ffcc00", "#cc00cc", "#ff4d4d", "#00cc00", "#b35900", "#4d4dff", "#808080", "#FF9933", "#99E6E6", "#FF99CC", "#6666FF", "#FFCC70", "#CC00CC")
+names(colors_vec) <- c("Costa Rica", "Trinidad y Tabago", "San Vicente y las Granadinas", "Colombia", "España", "Granada", "Jamaica", "Nicaragua", "Puerto Rico", "República Dominicana", "Santa Lucía", "Suriname", "Dominica", "Saint Kitts y Nevis", "Islas Vírgenes Británicas", "Anguila", "Antigua y Barbuda", "Guyana", "Montserrat", "Chile", "Paraguay", "Uruguay", "Perú", "Honduras", "Barbados", "Belice", "El Salvador", "Argentina", "Venezuela (República Bolivariana de)", "Ecuador", "Portugal", "Cuba", "Panamá")
 
+#Grafico de linea con el valor absoluto
+line_absoluto = data %>%
+  filter(tipodevalor == 'Número absoluto') %>%
+  ggplot(aes(x = anio, y = valor, color = paises)) +
+  geom_line(aes(group = paises)) +
+  scale_color_manual(values = colors_vec) +
+  labs(title = "Tendencia en numero de muertes de mujeres ocasionadas por parejas y  ex-parejas",
+       x = "Año",
+       y = "Numero de muertes") 
+
+#Grafico de linea con la tasa de muertes, por cada 100000 mujeres
+line_tasa = data %>%
+  filter(tipodevalor == 'Tasa (por cada 100.000 mujeres)') %>%
+  ggplot(aes(x = anio, y = valor, color = paises)) +
+  geom_line(aes(group = paises)) +
+  scale_color_manual(values = colors_vec) +
+  labs(title = "Tendencia en la tasa de muertes de mujeres ocasionadas por parejas y  ex-parejas",
+       x = "Año",
+       y = "Tasa (por cada 100.000 mujeres)") 
+
+#Mostrar ambos graficos juntos
+gridExtra::grid.arrange(line_absoluto, line_tasa, newpage = TRUE)
 
